8253a358af63a928466f77b8f8ed0427c5a2d501
Margaux <margaux.cavagna@hotmail.fr>
2020-04-16 16:33:10 +0200

    Page de sélection de l'oeuvre d'art

--- front/SMART/Components/ArtworkChoice.js
+++ front/SMART/Components/ArtworkChoice.js
@@ -1,17 +1,72 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, Button, TouchableOpacity, Card } from 'react-native'
 
-class ArtworkChoice extends React.Component{
+class ArtworkChoice extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Text>Vous souhaitez ajouter</Text>
-              <Button title="Un texte"></Button>
-              <Button title="Une oeuvre d'art existante"></Button>
-              <Button title="Annuler"></Button>
+            <View style={styles.container}>
+                <Text style={styles.Text}>Vous souhaitez ajouter</Text>
+
+                <TouchableOpacity style={styles.Button}>
+                    <View style={styles.otherView}>
+                        <Text style={styles.buttonText}>Un texte</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.Button}>
+                    <View style={styles.otherView}>
+                        <Text style={styles.buttonText}>Une oeuvre d'art existante</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.cancelButton}>
+                    <View style={styles.otherView}>
+                        <Text style={styles.buttonText}>Annuler</Text>
+                    </View>
+                </TouchableOpacity>
+
             </View>
-          );
-        }
+
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, alignItems: 'center', justifyContent: 'center', height: 500, width: "100%"
+    },
+    otherView: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlignVertical: 'center',
+    },
+    Text: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: "5%",
+        textAlign: "center",
+        color: "black",
+    },
+    Button: {
+        height: "10%",
+        width: "85%",
+        marginBottom: "4%",
+        backgroundColor: "orange",
+        borderRadius: 15,
+    },
+    cancelButton: {
+        height: "5%",
+        width: "75%",
+        backgroundColor: "red",
+        borderRadius: 15,
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "white",
+    }
+});
+
 
 export default ArtworkChoice;
