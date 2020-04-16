813e7a148c61e4eba1886ceedf873511a7fa128c
YoanSimco <yoan.simiand.cossin@free.fr>
2020-04-16 16:00:39 +0200

    Exported navigation from app.js

--- front/SMART/App.js
+++ front/SMART/App.js
@@ -1,22 +1,10 @@
-// In App.js in a new project
-
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import ArtworkChoice from './Components/ArtworkChoice'
-
-const Stack = createStackNavigator();
+import * as React from 'react'
+import Navigation from './Navigation/Navigation'
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="ArtworkChoice" component={ArtworkChoice} options={{ title: 'Choix de l\'oeuvre' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <Navigation/>
+  )
 }
 
-export default App;
+export default App
