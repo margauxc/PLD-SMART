e46f6995fb97be29fd362bfd575649ff45ccf57b
Margaux <margaux.cavagna@hotmail.fr>
2020-04-16 11:07:08 +0200

    Mise en place de la navigation

---
+++ front/SMART/Components/FilmDetail.js
@@ -0,0 +1,16 @@
+// Components/FilmDetail.js
+
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+class FilmDetail extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+    )
+  }
+}
+
+export default FilmDetail
