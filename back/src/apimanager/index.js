3bf44406a8c55794ceeee887fb7c3c15d38481e8
Marius Lelouard <lelmarius18@gmail.com>
2020-04-16 13:18:56 +0200

    first api route on swagger and transition to openapi 3.0.0

--- back/src/apimanager/index.js
+++ back/src/apimanager/index.js
@@ -1,8 +1,8 @@
-const spotify = require("spotify.js")
+const spotify = require("./spotify")
 const apis = [spotify]
 
 module.exports = {
-    apis.filter((x) => {
-        return x.type =='music'
-    }).forEach()
+    // apis.filter((x) => {
+    //     return x.type =='music'
+    // }).forEach()
 };
