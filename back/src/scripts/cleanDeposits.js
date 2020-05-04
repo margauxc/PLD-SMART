be9ce0586cf7a2afdf911bd36f435999c3928dca
Marius Lelouard <lelmarius18@gmail.com>
2020-05-04 10:40:43 +0200

    cron

---
+++ back/src/scripts/cleanDeposits.js
@@ -0,0 +1,28 @@
+const config = require('../config');
+const Logger = require('../loaders/logger')
+const dbLoader = require('../loaders/db-sequelize')
+const {models} = require('../models')
+const { Op } = require("sequelize");
+const moment = require('moment')
+
+async function run() {
+    // Logger.debug(`${process.env.NODE_ENV}`)
+    await dbLoader()
+    Logger.info(`**********
+    begin clearing databse from deprecated deposits
+    **********`)
+    const nbSuppr = await models.Deposit.destroy({
+        where: {
+            expirationDate : 
+            {
+                [Op.lte]: moment().add(1,'days').toDate()
+            }
+        }
+    })
+    // const nbSuppr = await deprecatedDeposits.forEach.destroy()
+    Logger.info(`${nbSuppr} deposits deleted`)
+}
+
+run()
+
+// */5 * * * * node ./src/scripts/cleanDeposits
