a269ab66ad586be9e5c5d5cac8c17d3fad7816e1
manonmra <manon.moreira@insa-lyon.fr>
2020-05-06 10:16:20 +0200

    conditional display name

--- front/SMART/Components/Name.js
+++ front/SMART/Components/Name.js
@@ -6,22 +6,26 @@ class Name extends React.Component{
 
     constructor(props){
         super(props)
-    }
-
-    _onPress(){
-        DefaultPreferences.set('username', 'Manon').then(()=> {
-            DefaultPreferences.get('username').then((username) => {
-                console.log(username)
-            })
-        }).catch((error) => console.log(error))
-        this.props.navigation.navigate("Home")
+        this.state = {usernameSet : false}
+        this.username = ""
     }
 
     render(){
+        if(!this.state.usernameSet){
+            DefaultPreferences.get('username').then((username) => {
+                if(username != null && username.length > 0){
+                    this.username = username
+                    this.setState({usernameSet : true})
+                }
+            })
+        }
         return (
             <View style = {{flex:1}}>
-                <Text>Ceci est la page Name</Text>
-                <Button title = "Go to Home" onPress = {() => this._onPress()}/>
+                {this.state.usernameSet ? (
+                    <Text>Bienvenue {this.username} !</Text>
+                ) : (
+                    <Text>Il faut entrer un nom</Text>
+                )}
             </View>
         )
     }
